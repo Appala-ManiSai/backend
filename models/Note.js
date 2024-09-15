1b448f7cef6236fa3914d9c04c498b0b04684442
Appala-ManiSai <manisaiyadav666@gmail.com>
2024-09-15 22:24:19 +0530

    addnote

--- models/Note.js
+++ models/Note.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
-
+const {Schema}=mongoose;
 const UserSchema = new Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId, 
+        ref: 'user'
+    },
     title:{
         type:String,
         required:true
     },
     description:{
         type:String,
-        required:true,
-        unique:true
+        required:true
     },
     tag:{
         type:String,
